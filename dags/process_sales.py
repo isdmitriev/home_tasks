d8f4f1bc51235d6bf268a0bee32fd227574d0c5f
isdmitriev <isdmitriev2@gmail.com>
2024-01-07 17:03:25 +0300

    dags_change_tasks

--- dags/process_sales.py
+++ dags/process_sales.py
@@ -10,7 +10,7 @@ def extract_data_from_api():
     interesting_dates: List[str] = ["2022-08-09", "2022-08-10", "2022-08-11"]
     for date in interesting_dates:
         raw_dir: str = f"C:\\tasks\\data_storage\\raw\\sales\\{date}"
-        uri: str = "http://localhost:5000/"
+        uri: str = "http://host.docker.internal:5000/"
         post_data = {"raw_dir": raw_dir, "date": date}
         response: Response = requests.post(uri, json=post_data)
         if (response.status_code == 500):
@@ -19,7 +19,7 @@ def extract_data_from_api():
 
 def convert_to_avro():
     interesting_dates: List[str] = ["2022-08-09", "2022-08-10", "2022-08-11"]
-    uri: str = "http://localhost:5001"
+    uri: str = "http://host.docker.internal:5001/"
     for date in interesting_dates:
         stg_dir: str = f"C:\\tasks\\data_storage\\stg\\sales\\{date}"
         raw_dir: str = f"C:\\tasks\\data_storage\\raw\\sales\\{date}"
@@ -36,5 +36,5 @@ with DAG(dag_id="process_sales",
          schedule_interval=timedelta(minutes=2),
          catchup=False) as dag:
     task1 = PythonOperator(task_id="extract_data_from_api", python_callable=extract_data_from_api)
-    task2 = PythonOperator(task_id="convert_to_avro", depends_on_past=True, python_callable=convert_to_avro)
+    task2 = PythonOperator(task_id="convert_to_avro", python_callable=convert_to_avro)
 task1 >> task2
